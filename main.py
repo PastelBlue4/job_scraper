63dadcc1a2441bcfb9c7cea3f04ca4be16d9fcfd
Romuru <pastelblue0721@gmail.com>
2023-03-04 23:59:35 +0900

    feat setup test

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from requests import get
+
 from bs4 import BeautifulSoup
+
+
+base_url = "https://search.shopping.naver.com/search/all?query=%EA%B0%84%EC%9E%A5%EA%B2%8C%EC%9E%A5"
+
+
+response = get(f"{base_url}")
+
+if response.status_code != 200:
+    print(response.text)
+else:
+    print(response.text)
