c902435f5d81938422e96df6b885b35861573b01
Romuru <pastelblue0721@gmail.com>
2023-03-05 14:41:08 +0900

    feat. refactor

---
+++ extractors/wwr.py
@@ -0,0 +1,18 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def extract_wwr_jobs(keyword):
+
+    main_url = "https://weworkremotely.com/remote-jobs/search?term="
+
+    response = get(f"{main_url}{keyword}")
+    if response.status_code != 200:
+        print("정보 받아오기 실패")
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        print(soup.prettify())
+
+        # jobs = soup.find_all("section", class_="jobs")
+        # print(len(jobs))
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from requests import get
-
 from bs4 import BeautifulSoup
 
+from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://search.shopping.naver.com/search/all?query=%EA%B0%84%EC%9E%A5%EA%B2%8C%EC%9E%A5"
 
-
-response = get(f"{base_url}")
-
-if response.status_code != 200:
-    print(response.text)
-else:
-    print(response.text)
+result = extract_wwr_jobs("python")
+print(result)
