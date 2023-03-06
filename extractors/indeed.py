46b1a3857e9e44a3215a18ab9cd015267323821d
Romuru <pastelblue0721@gmail.com>
2023-03-07 01:53:59 +0900

    feat data to need scraping

--- extractors/indeed.py
+++ extractors/indeed.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 
 browser = webdriver.Chrome()
 
-browser.get("https://kr.indeed.com/jobs?q=python")
+browser.get("https://kr.indeed.com/jobs?q=react")
 
 soup = BeautifulSoup(browser.page_source, "html.parser")
 
@@ -20,9 +20,22 @@ jobs = job_list.find_all("li", recursive=False)
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
     if zone == None:
-        print("job li")
-    else:
-        print("mosac li")
+        anchor = job.select_one("h2 a")
+
+        title = anchor.find("span").string
+        preview_container = job.find("div", class_="result-footer")
+
+        create_at = preview_container.find("span", class_="date")
+        create_at.span.extract()
+        extract_create_at = create_at.string
+
+        preview = preview_container.select_one("div").string
+        if preview == None:
+            preview = "미리보기 정보가 없어요."
+
+        info_container = job.find("div", class_="company_location")
+        company_name = info_container.select_one("span").string
+        company_location = info_container.select_one("div").string
 
 
 while True:
