305bb5f996a654c0573ba7661eb28175d84c9e85
Romuru <pastelblue0721@gmail.com>
2023-03-07 02:49:46 +0900

    feat save to csv, install web_driver
    scrap done:)

--- extractors/indeed.py
+++ extractors/indeed.py
@@ -1,42 +1,54 @@
-from requests import get
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-browser = webdriver.Chrome()
+def extract_ind_jobs(keyword):
 
-browser.get("https://kr.indeed.com/jobs?q=react")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
 
-soup = BeautifulSoup(browser.page_source, "html.parser")
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=chrome_options
+    )
+    browser.get(f"https://kr.indeed.com/jobs?q={keyword}")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
+    results = []
 
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all("li", recursive=False)
 
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
+        if zone == None:
+            anchor = job.select_one("h2 a")
 
-jobs = job_list.find_all("li", recursive=False)
+            title = anchor.find("span").string
+            link = anchor["href"]
+            preview_container = job.find("div", class_="result-footer")
 
+            create_at = preview_container.find("span", class_="date")
+            create_at.span.extract()
+            extract_create_at = create_at.string
 
-for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
-    if zone == None:
-        anchor = job.select_one("h2 a")
+            preview = preview_container.select_one("div").string
+            if preview == None:
+                preview = "미리보기 정보가 없어요."
 
-        title = anchor.find("span").string
-        preview_container = job.find("div", class_="result-footer")
+            info_container = job.find("div", class_="company_location")
+            company_name = info_container.select_one("span").string
+            company_location = info_container.select_one("div").string
 
-        create_at = preview_container.find("span", class_="date")
-        create_at.span.extract()
-        extract_create_at = create_at.string
+            job_data = {
+                "company_name": company_name.replace(",", " "),
+                "location": company_location.replace(",", " "),
+                "detail_info": preview.replace(",", " "),
+                "create_at": extract_create_at.replace(",", " "),
+                "link": f"https://kr.indeed.com{link}".replace(",", " "),
+            }
+            results.append(job_data)
 
-        preview = preview_container.select_one("div").string
-        if preview == None:
-            preview = "미리보기 정보가 없어요."
-
-        info_container = job.find("div", class_="company_location")
-        company_name = info_container.select_one("span").string
-        company_location = info_container.select_one("div").string
-
-
-while True:
-    pass
+    return results
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from requests import get
-from bs4 import BeautifulSoup
+from extractors.indeed import extract_ind_jobs
 
-from extractors.wwr import extract_wwr_jobs
+keyword = input("검색 키워드를 입력해주세요.")
 
 
-result = extract_wwr_jobs("python")
-print(result)
+results = extract_ind_jobs(keyword)
+
+file = open(f"{keyword}.csv", "w", encoding="utf-8")
+file.write("회사 이름, 위치, 정보, 공고 업로드 시간, 링크\n")
+
+for result in results:
+    file.write(
+        f"{result['company_name']},{result['location']},{result['detail_info']},{result['create_at']},{result['link']}\n"
+    )
+
+file.close()
